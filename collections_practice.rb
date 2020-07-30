523ce67b4c8e6cb066c42b7e3b774f381d8bfc3c
Anthony Lepore <violacellonyc@gmail.com>
2020-07-30 19:53:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a 
+end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  a = array[1]
+  b = array[2]
+  array[1] = b 
+  array[2] = a
+  array
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |i|
+   i[2] = "$"
+   new_array.push(i)
+  end
+  new_array
+end
+
+def find_a(array)
+  new_array = []
+  array.find_all do |i|
+    if i.start_with?("a")
+      new_array.push(i)
+    end
+  end
+  new_array
+end
 
